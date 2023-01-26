82e9945238382663da4f40bbeed4e181ba761b95
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 08:07:39 +0300

    ilk ders bitti.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,34 @@ import { Component } from 'react';
 class App extends Component {
    render()
    {
+    const sayi = 25;
+    const dogrumu = true
     return (
+
+   
+
       <div className="App">
         Merhaba Dünya
+
+        <h1>Vepa</h1>
+
+        <label htmlFor=""></label>
+        <h4>{2+2}</h4>
+
+       { "vepa".toUpperCase()}
+
+       <h4>{sayi}</h4>
+
+       {
+        dogrumu ? <p>Kullanici Var</p>  : <p>Kullanici Yok</p>
+       }
+
       </div>
+
+ 
+      
+
+      
 
       
     );
