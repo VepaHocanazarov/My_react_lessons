683d8f0538858d080a975dda4d25c927c7c98334
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 11:49:29 +0300

    React'e css ve bootstrap ekleme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
-import './App.css';
+// import './App.css';
 import { Component } from 'react';
 import Vepa from './Vepa';
+import './deneme.css'
+
 
 
 class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        Merhaba Dünya
+      <div className="container">
+       <h1>Merhaba Dünya</h1>
 
         <Vepa />
 
       </div>
-
+ 
     );
   }
 }
