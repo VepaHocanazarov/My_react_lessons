1fc66d926f796cc2d150183b7d69c7b1032edf96
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 09:17:03 +0300

    vepa.js oluşturuldu

---
+++ src/Vepa.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react'
+
+class Vepa extends Component {
+  render() {
+    return (
+      <div>
+
+        <h1>React in daha başindayiz</h1>
+        <form action="">
+          <input type="text" />
+          <button>Kaydet</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Vepa;
