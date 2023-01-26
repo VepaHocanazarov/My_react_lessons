472d29bd052887d4da0f8add2ac6bfa43f6ebc31
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 08:57:35 +0300

    ilk komponent oluşturuldu

--- src/App.js
+++ src/App.js
@@ -2,73 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
+
 class App extends Component {
-  render() {
-
+   render()
+   {
     const sayi = 25;
-
     const dogrumu = true
     return (
+
+
+
       <div className="App">
         Merhaba Dünya
 
         <h1>Vepa</h1>
 
-        <label For=""></label>
+        <label htmlFor=""></label>
+        <h4>{2+2}</h4>
 
-        <h4> {2 + 2} </h4>
+       { "vepa".toUpperCase()}
 
-        {"Vepa".toUpperCase()}
+       <h4>{sayi}</h4>
 
-        <h3>{sayi}</h3>
-
-        {
-          dogrumu ? <p>Kullanici var</p>  : <p>Kullanici yok</p>
-        }
-
-
+       {
+        dogrumu ? <p>Kullanici Var</p>  : <p>Kullanici Yok</p>
+       }
 
       </div>
-    )
+
+    );
   }
-}
-
-// class App extends Component {
-//    render()
-//    {
-//     const sayi = 25;
-//     const dogrumu = true
-//     return (
-
-
-
-//       <div className="App">
-//         Merhaba Dünya
-
-//         <h1>Vepa</h1>
-
-//         <label htmlFor=""></label>
-//         <h4>{2+2}</h4>
-
-//        { "vepa".toUpperCase()}
-
-//        <h4>{sayi}</h4>
-
-//        {
-//         dogrumu ? <p>Kullanici Var</p>  : <p>Kullanici Yok</p>
-//        }
-
-//       </div>
-
-
-
-
-
-
-
-//     );
-//   }
-//    }
+   }
 
 
 export default App;
