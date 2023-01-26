b80dd3cf39f8516d9b8154cb10c7f9b2b2d01565
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 08:41:38 +0300

    first lesson practice

--- src/App.js
+++ src/App.js
@@ -3,41 +3,72 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-   render()
-   {
+  render() {
+
     const sayi = 25;
+
     const dogrumu = true
     return (
-
-   
-
       <div className="App">
         Merhaba Dünya
 
         <h1>Vepa</h1>
 
-        <label htmlFor=""></label>
-        <h4>{2+2}</h4>
+        <label For=""></label>
 
-       { "vepa".toUpperCase()}
+        <h4> {2 + 2} </h4>
 
-       <h4>{sayi}</h4>
+        {"Vepa".toUpperCase()}
 
-       {
-        dogrumu ? <p>Kullanici Var</p>  : <p>Kullanici Yok</p>
-       }
+        <h3>{sayi}</h3>
+
+        {
+          dogrumu ? <p>Kullanici var</p>  : <p>Kullanici yok</p>
+        }
+
+
 
       </div>
-
- 
-      
-
-      
-
-      
-    );
+    )
   }
-   }
- 
+}
+
+// class App extends Component {
+//    render()
+//    {
+//     const sayi = 25;
+//     const dogrumu = true
+//     return (
+
+
+
+//       <div className="App">
+//         Merhaba Dünya
+
+//         <h1>Vepa</h1>
+
+//         <label htmlFor=""></label>
+//         <h4>{2+2}</h4>
+
+//        { "vepa".toUpperCase()}
+
+//        <h4>{sayi}</h4>
+
+//        {
+//         dogrumu ? <p>Kullanici Var</p>  : <p>Kullanici Yok</p>
+//        }
+
+//       </div>
+
+
+
+
+
+
+
+//     );
+//   }
+//    }
+
 
 export default App;
