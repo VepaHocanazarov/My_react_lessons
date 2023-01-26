68fac1c2e7e81fd7f51fc2ba09d51f0a3f3936e6
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-26 13:02:08 +0300

    Functional component (4.ders)

---
+++ src/Aygul.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+ function Aygul() {
+  return (
+    <div>
+
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti aspernatur, inventore mollitia eligendi in incidunt repellendus temporibus aperiam sequi.</h3>
+      
+    </div>
+  )
+}
+
+export default Aygul;
+
+// Degişecek alanlar için functional componenti kullanamıyoruz. Burda sadece propusları gönderbiliyoruz fakat statesleri kullanamıyoruz 
